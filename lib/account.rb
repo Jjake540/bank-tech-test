fe8ed3f39e048909ab146ec1da8acfd536a4303a
jake <jakebs540@gmail.com>
2021-10-25 22:24:12 +0100

    transactions array saves, date, credit, debit and balance

--- lib/account.rb
+++ lib/account.rb
@@ -3,17 +3,27 @@ require_relative 'statement'
 class Account
   
   attr_reader :balance
+  attr_reader :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    @transactions.push({"debit" => '   ', "credit" => amount, "balance" => @balance, "date" => date})
   end
 
   def withdraw(amount)
     @balance -= amount
+    @transactions.push({"debit" => amount, "credit" => '   ', "balance" => @balance, "date" => date})
   end
 
+private
+
+  def date
+    Statement.new.timestamp
+  end
+  
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -19,5 +19,35 @@ describe Account do
     account.withdraw(50)
     expect(account.balance).to eq(50)
   end
+  
+  describe 'transactions' do
+    it 'stores a transaction after a deposit' do
+      account = Account.new
+      account.deposit(50)
+      account.deposit(100)
+      account.deposit(150)
+      expect(account.transactions.length).to eq(3)
+    end
+
+    it 'stores a transaction after a withdraw' do
+      account = Account.new
+      account.withdraw(50)
+      account.withdraw(50)
+      expect(account.transactions.length).to eq(2)
+    end
+
+    it 'transaction updates with balance' do
+      account = Account.new
+      account.deposit(50)
+      expect(account.transactions[0]['balance']).to eq(50)
+    end
+
+    it 'transaction updates with date' do
+      account = Account.new
+      account.deposit(50)
+      date = Statement.new.timestamp
+      expect(account.transactions[0]['date']).to eq(date)
+    end
+  end
 
 end
